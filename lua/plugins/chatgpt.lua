1d6a79afcbcedfc8401b9184de4d4e9324fdc626
Ruby-PL <123518084+Ruby-PL@users.noreply.github.com>
2025-09-02 13:34:06 +0200

    Update chatgpt.lua

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -4,7 +4,7 @@ return {
     event = "VeryLazy",
     config = function()
       require("chatgpt").setup({
-        api_key_cmd = "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",
+        api_key_cmd = "API_KEY",
         openai_params = {
           model = "meta-llama/llama-3.3-70b-instruct:free", -- of "claude-3-sonnet", enz.
         },
